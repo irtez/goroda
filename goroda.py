789292db802252f9fca83f1f628f2c6d27ada063
irtez <lokipolik32@mail.ru>
2022-09-04 17:25:08 +0300

    Update goroda.py

--- goroda.py
+++ goroda.py
@@ -1,5 +1,5 @@
 cityes_old = []
-symbols_bad = {"ь","ъ","ц","й"}
+symbols_bad = {"ь","ъ"} #1 ошибка
   
   
 text1 = open("города.txt")
@@ -23,6 +23,7 @@ s_end = 'a'
 step = 'human'
 cityes_old.append(city)
 s_end = city[-1]
+s_start = city[0]
   
 while game_over == False:    
   
@@ -31,16 +32,18 @@ while game_over == False:
         correct = False
         while correct == False:
               
-            city = input("Введите ваш город: на букву: " + s_end+". Ваш город: ")
+            city = input("Введите ваш город: на букву: " + s_end + ". Ваш город: ")
             if city == "стоп":
                 game_over = True
                 correct = True
             else:
                 correct = True
                 #Проверить что город на нужную букву
-                if city[0].lower () != s_end:
+                if city[0].lower() != s_end and city[-1].lower() != s_end and city[0].lower() != s_start.lower(): #2 и 4 ошибки 
                     correct = False
                     print("Не верно. Назовите город на букву",s_end)
+
+                
   
                 #Проверить что такой город существует
                 if city in set(cityes_all):
@@ -58,13 +61,11 @@ while game_over == False:
     else:
           
         city = ''
-        for city_next in cityes:            
+        for city_next in cityes:        #6 ошибка (выбирается всегда последний по алфавиту город на заданную букву)
             if city_next[0].lower() == s_end:
                 city = city_next
   
-        if city == '':
-            print('Вы победили')
-            print('Не найден город на букву', s_end)
+        if city == '': #3 ошибка
             game_over = True
         else:
             print(city)
@@ -73,8 +74,8 @@ while game_over == False:
         step = 'human'
   
     if game_over == False:
-        cityes.remove(city)
-        cityes_old.append(city)
+        #cityes.remove(city)     #5 ошибка
+        #cityes_old.append(city) 
   
         s_end = city[-1]
         if s_end in symbols_bad:
@@ -82,8 +83,12 @@ while game_over == False:
   
         if s_end in symbols_bad:
             s_end = city[-3]
+
+        s_start = city[0]
     else:
         pass
           
+print('Вы победили')    #3 ошибка
+print('Не найден город на букву', s_end)
 print('Игра окончена')
 print('Назвали ', len(cityes_old), " городов из ", len(cityes_all))
