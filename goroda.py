206d44b7145aafe16237a934a659bc014338dcda
irtez <lokipolik32@mail.ru>
2022-09-06 22:37:17 +0300

    Update goroda.py

--- goroda.py
+++ goroda.py
@@ -1,5 +1,27 @@
+def cityCheck(city, cityes_all, cityes_old, s_end):
+    #Проверить что город на нужную букву
+    if city[0].lower() != s_end:
+        correct = 1
+        return correct
+    # Проверить что такой город существует
+    if city not in set(cityes_all):
+        correct = 2
+        return correct
+    # Проверить что ранее этот город не называли
+    if city in set(cityes_old):
+        correct = 3
+        return correct
+    correct = 0
+    return correct
+
+def loseCheck(s_end, cityes):
+    for city in cityes:
+        if city[0].lower() == s_end:
+            return True
+    return False
+
 cityes_old = []
-symbols_bad = {"ь","ъ"} #1 ошибка
+symbols_bad = {"ь","ъ","ы","ц","й"}
   
   
 text1 = open("города.txt")
@@ -17,78 +39,127 @@ game_over = False
   
 print("Игра в города. Что бы закончить игру введите слово стоп")
   
-city = 'Москва'
-print(city)
-s_end = 'a'
-step = 'human'
-cityes_old.append(city)
-s_end = city[-1]
-s_start = city[0]
-  
-while game_over == False:    
-  
-    if step == 'human':
-  
-        correct = False
-        while correct == False:
-              
-            city = input("Введите ваш город: на букву: " + s_end + ". Ваш город: ")
-            if city == "стоп":
-                game_over = True
-                correct = True
-            else:
-                correct = True
-                #Проверить что город на нужную букву
-                if city[0].lower() != s_end and city[-1].lower() != s_end and city[0].lower() != s_start.lower(): #2 и 4 ошибки 
-                    correct = False
-                    print("Не верно. Назовите город на букву",s_end)
 
-                
-  
-                #Проверить что такой город существует
-                if city in set(cityes_all):
-                    pass
+mode = input('Выберите режим игры:\n1. Игра с компьютером.\n2. Игра вдвоём.\n3. Выйти из игры.\n')
+
+if mode == '1':
+    print('Выбран режим игры с компьютером.')
+    city = 'Москва'
+    print(city)
+    step = 'human'
+    
+    s_end = city[-1]
+    while not game_over:
+        if step == 'human':
+            correct = False
+            while correct == False:
+                city = input("Введите ваш город: на букву: " + s_end + ". Ваш город: ")
+                if city == "стоп":
+                    game_over = True
+                    correct = True
                 else:
                     correct = False
-                    print("Не верно. Такого города не существует")                
-  
-                #Проверить что ранее этот город не называли
-                if city in set(cityes_old):
-                    correct = False
-                    print("Не верно. Такой город уже называли")    
-  
-        step = 'AI'
-    else:
-          
-        city = ''
-        for city_next in cityes:        #6 ошибка (выбирается всегда последний по алфавиту город на заданную букву)
-            if city_next[0].lower() == s_end:
-                city = city_next
-  
-        if city == '': #3 ошибка
-            game_over = True
+                    if cityCheck(city, cityes_all, cityes_old, s_end) == 1:
+                        print("Не верно. Назовите город на букву", s_end)
+                    elif cityCheck(city, cityes_all, cityes_old, s_end) == 2:
+                        print("Не верно. Такого города не существует")
+                    elif cityCheck(city, cityes_all, cityes_old, s_end) == 3:
+                        print("Не верно. Такой город уже называли")
+                    else:
+                        correct = True
+            step = 'AI'
         else:
-            print(city)
-              
-              
-        step = 'human'
-  
-    if game_over == False:
-        #cityes.remove(city)     #5 ошибка
-        #cityes_old.append(city) 
-  
+            city = ''
+            for city_next in cityes:
+                if city_next[0].lower() == s_end:
+                    city = city_next
+            if city == '':
+                print('Вы победили')
+                print('Не найден город на букву', s_end)
+                game_over = True
+            else:
+                print(city)
+            step = 'human'
+
+        if game_over == False:
+            cityes.remove(city)
+            cityes_old.append(city)
+
+            s_end = city[-1]
+            if s_end in symbols_bad:
+                s_end = city[-2]
+
+            if s_end in symbols_bad:
+                s_end = city[-3]
+
+            if not loseCheck(s_end, cityes):
+                print(f'Больше городов на букву {s_end} не осталось. Вы проиграли.')
+                game_over = True
+        else:
+            pass
+
+else: 
+    print('Вы выбрали выход из игры.')
+    i = 0
+    correct = False
+
+    while not correct:
+        city = input('Введите любой город для начала игры.\n')
+        if city == 'стоп ':
+            game_over = True
+            correct = True
+        else:
+            if city in cityes:
+                correct = True
+            else:
+                print("Не верно. Такого города не существует")
+
+    if not game_over:
         s_end = city[-1]
         if s_end in symbols_bad:
             s_end = city[-2]
-  
         if s_end in symbols_bad:
             s_end = city[-3]
+        cityes.remove(city)
+        cityes_old.append(city)
 
-        s_start = city[0]
-    else:
-        pass
+    while not game_over:
+        i += 1
+
+        correct = False
+
+        while correct == False:
+            city = input("Введите ваш город: на букву: " + s_end + ". Ваш город: ")
+            if city == 'стоп':
+                game_over = True
+                correct = True
+            else:
+                if cityCheck(city, cityes_all, cityes_old, s_end) == 1:
+                    print("Не верно. Назовите город на букву", s_end)
+                elif cityCheck(city, cityes_all, cityes_old, s_end) == 2:
+                    print("Не верно. Такого города не существует")
+                elif cityCheck(city, cityes_all, cityes_old, s_end) == 3:
+                    print("Не верно. Такой город уже называли")
+                else:
+                    correct = True
+
+        if game_over == False:
+            cityes.remove(city)
+            cityes_old.append(city)
+
+            s_end = city[-1]
+            if s_end in symbols_bad:
+                s_end = city[-2]
+
+            if s_end in symbols_bad:
+                s_end = city[-3]
+
+            if not loseCheck(s_end, cityes):
+                print(f'Больше городов на букву {s_end} не осталось. Выиграл {(i + 2) + 1} игрок.')
+                game_over = True
+        else:
+            pass
+
           
-print('Вы победили')    #3 ошибка
-print('Не найден город на букву', s_end)
 print('Игра окончена')
 print('Назвали ', len(cityes_old), " городов из ", len(cityes_all))
